0b8f87e7e5c65b0288fe3b77a862abcd56a8fc4a
Juanique Voogt <juanique@kartoza.com>
2024-03-04 13:06:21 +0200

    dockerize app

--- api/app.py
+++ api/app.py
@@ -48,3 +48,7 @@ def help():
         """
     }
     return jsonify(data)
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 7000))
+#     app.run(debug=False, host='0.0.0.0', port=port)
